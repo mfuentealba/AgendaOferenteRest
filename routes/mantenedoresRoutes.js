7bc27d6496abed21425222c2e7e7db5c464bb24d
mario fuentealba otarola <mario.fuentealba.otarola@gmail.com>
2018-05-14 00:28:27 -0400

    Completando el modelo

--- routes/mantenedoresRoutes.js
+++ routes/mantenedoresRoutes.js
@@ -9,14 +9,7 @@ var md_auth = require('../middlewares/authenticated');
 var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir: './uploads/users'})
 
-api.get('/home', UserController.fnHome);
-api.get('/pruebas', md_auth.ensureAuth, UserController.fnPruebas);
-api.post('/registerUser'/*, md_auth.ensureAuth*/, UserController.saveUser);
-api.post('/loginUser', UserController.loginUser);
-api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
-api.get('/users/:page?', md_auth.ensureAuth, UserController.getUsers);
-api.put('/userUpdate/:id', md_auth.ensureAuth, UserController.updateUser);
-api.post('/uploadImageUser/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
-api.get('/getImageFile/:imageFile', UserController.getImageFile);
-api.get('/counters/:id?', md_auth.ensureAuth, UserController.getCounters);
+api.post('/tipo-contrato/create/', md_auth.ensureAuth, UserController.uploadImage);
+api.post('/clasificacion/create/', md_auth.ensureAuth, UserController.uploadImage);
+api.post('/subclasificacion/create/', md_auth.ensureAuth, UserController.uploadImage);
 module.exports = api;
